23665d79759009141039f7ba3a2f79935be7f55b
Bryan-zfc <bryanadvo@hotmail.com>
2026-03-12 15:38:07 +1100

    Update dashboard.py

--- frontend/dashboard.py
+++ frontend/dashboard.py
@@ -1,62 +1,74 @@
+# How to run:
+# First run back end in terminal: uvicorn backend.server:app --reload
+# Run frontend in new terminal: streamlit run frontend/dashboard.py
+
 import streamlit as st
 import requests
 import pandas as pd
-import datetime
 
 st.title("Language Study Dashboard")
 
-try:
-    existing_languages = requests.get("http://127.0.0.1:8000/languages").json()
-except:
-    existing_languages = []
-
-# Add an option at the top for typing a new language
-existing_languages.insert(0, "")  # empty string will allow typing new
-
-# Single entry bar: select existing or type new
-language = st.selectbox("Language (pick or type new)", existing_languages)
-if language == "":
-    language = st.text_input("Enter new language")
-
-skill = st.selectbox(
-    "Skill",
-    ["Reading", "Listening", "Speaking", "Writing"]
-)
-
-duration = st.number_input("Minutes studied", 0, 300)
-date = st.date_input("Study date", datetime.date.today())
-
-if st.button("Log Study Session"):
-
-    requests.post(
-        "http://127.0.0.1:8000/add_session",
-        json={
-            "language": language,
-            "skill": skill,
-            "study_date": str(date),
-            "duration_minutes": duration
-        }
-    )
-    st.success("Session recorded!")
-
-    st.rerun()  
-
-
-
 data = requests.get("http://127.0.0.1:8000/sessions").json()
 
-df = pd.DataFrame(data, columns=["language", "skill", "date", "minutes"])
+df = pd.DataFrame(
+    data,
+    columns=["language", "skill", "activity","study_date","duration_minutes"]
+)
 
 st.subheader("Study Sessions")
 
-st.dataframe(df)
+with st.expander("Click to see all study sessions"):
+    st.dataframe(df, 
+            hide_index=True,
+            column_config={
+                "language": st.column_config.Column(
+             label="Language", # The label shown in the UI
+             width="small"
+             ),
 
+            "skill": st.column_config.Column(
+             label="Skill", # The label shown in the UI
+             width="small",
+             help="Which language skill have you trained"
+             ),
+
+             "activity": st.column_config.Column(
+             label="Activity", # The label shown in the UI
+             width="medium",
+             help="What did you do this learning session?"
+             ),
+
+            "study_date": st.column_config.Column(
+             label="Study Date", # The label shown in the UI
+             width="small"
+             ),
+            "duration_minutes": st.column_config.Column(
+             label="Session Duration", # The label shown in the UI
+             width="small",
+             ),
+            
+            }       
+    )
+
+
+# Show a barchart with the total hours studied per language
 st.subheader("Total Study Time by Language")
 
-chart = df.groupby("language")["minutes"].sum()
+chart = df.groupby("language")["duration_minutes"].sum() /60
+st.bar_chart(chart, y_label = "Total Hours")
+
+
+# Show a table with all the read books
+data_books = requests.get("http://127.0.0.1:8000/books").json()
+
+df = pd.DataFrame(data_books, columns=["title", "author", "language", "pages", "finish_date"])
+
+st.subheader("Finished Books")
+
+st.dataframe(df)
+
+st.subheader("Total pages read by Language")
+
+chart = df.groupby("language")["pages"].sum()
 
 st.bar_chart(chart)
-
-# How to run: cd language_dashboard
-# First run front end in terminal: uvicorn backend.server:app --reload
-# Run frontend in new terminal: streamlit run frontend/dashboard.py
